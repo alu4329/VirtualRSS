3ce8e3cf85d3bbe8925db4d009e56444a3e0c9d7
EduBP <edu_bp89@hotmail.com>
2013-11-30 16:37:42 +0000

    Acortado tamaño de descripción

--- lib/noko.rb
+++ lib/noko.rb
@@ -3,10 +3,21 @@ require 'pp'
 require 'open-uri'
 require 'ruby-bitly'
 
+  def truncate(text, length = 20, end_string = '…')
+    words = text.split()
+    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
+
   @doc = Nokogiri::XML(open("http://rss.nytimes.com/services/xml/rss/nyt/InternationalHome.xml"))
   @doc2 = Nokogiri::XML(open("http://ep00.epimg.net/rss/elpais/portada.xml"))
       
-  for i in 0..@doc.search('title').length - 10
-    puts @doc.search('title').children[i]
-    puts Bitly.shorten(@doc.search('link').children[i], "o_1qfb1am5b6", "R_770c5cbd981cadac42359a65fed206df").url
+  for i in 0..2
+    @doc.search('title').children[i]
+    if (@doc.search('description').children[i].text.length) > 27
+      #@doc.search('description').children[i].text = 
+      puts truncate(@doc.search('description').children[i].text)
+    
+    #puts Bitly.shorten(@doc.search('link').children[i], "o_1qfb1am5b6", "R_770c5cbd981cadac42359a65fed206df").url
+    end
   end
+  
