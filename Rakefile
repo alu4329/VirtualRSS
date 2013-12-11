7e0c642582e1d6aedd6cf0b7d6b9f8dcd67c20e9
EduBP <edu_bp89@hotmail.com>
2013-12-11 20:10:14 +0000

    fin por hoy

--- Rakefile
+++ Rakefile
@@ -1,6 +1,6 @@
 desc "run server"
 task :default do
-  sh "ruby lib/visualrss.rb"
+  sh "rackup"
 end
 
 desc "install dependencies"
---
+++ index.rb
@@ -0,0 +1,27 @@
+#require './lib/visualrss.rb'
+#require 'sinatra/flash'
+require 'dm-core'
+require 'dm-migrations'
+require 'sinatra'
+require 'sass'
+require 'pp'
+require 'open-uri'
+require 'nokogiri'
+require 'ruby-bitly'
+require 'haml'
+require './noko.rb'
+
+class User
+  include DataMapper::Resource
+  
+  property :id,       Serial
+  property :username, String
+  property :password, String
+  property :foto,     String
+  property :rss,      String
+end
+
+
+
+DataMapper.finalize
+
