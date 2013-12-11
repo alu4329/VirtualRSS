209632e7915a3b90a6237b2f1802c8e89ff5351b
Victor Plaza <M4tus3l4@hotmail.com>
2013-12-11 20:05:04 +0000

    Modificada la manera de trabajar con las rss, de array a hash

--- noko.rb
+++ noko.rb
@@ -5,14 +5,15 @@ require './words.rb'
   #@doc2 = Nokogiri::XML(open("http://ep00.epimg.net/rss/elpais/portada.xml"))
 def show_rss doc 
   @doc = Nokogiri::XML(open(doc))
-  @resultado = []
-  for i in 0..2
+  @resultado = {:title =>[], :link =>[]}
+  for i in 2..4
     @doc.search('title').children[i]
     if (@doc.search('description').children[i].text.length) > 10
       #@doc.search('description').children[i].text = 
-      @resultado.push("#{truncate(@doc.search('description').children[i].text)}")  
+      @resultado[:title].push(truncate(@doc.search('description').children[i].text)) 
+      @resultado[:link].push(@doc.search('link').children[i])
     #puts Bitly.shorten(@doc.search('link').children[i], "o_1qfb1am5b6", "R_770c5cbd981cadac42359a65fed206df").url
-      puts (@doc.search('link').children[i])
+      #puts (@doc.search('link').children[i])
     end
   end
   @resultado
